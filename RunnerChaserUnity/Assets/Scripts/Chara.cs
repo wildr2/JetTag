df510f38e0b331261b711cf693c30f03210b8689
Robert Wild <wildr2@rpi.edu>
2016-10-11 13:43:26 -0400

    Add powers

--- RunnerChaserUnity/Assets/Scripts/Chara.cs
+++ RunnerChaserUnity/Assets/Scripts/Chara.cs
@@ -7,6 +7,9 @@ public enum ControlScheme { WASD, Arrows }
 
 public class Chara : MonoBehaviour
 {
+    // Debug
+    public bool unlim_powers;
+
     // Player Info
     public int PlayerID { get; private set; }
     public Color PlayerColor { get; private set; }
@@ -18,13 +21,19 @@ public class Chara : MonoBehaviour
     public ParticleSystem bump_ps;
 
     // Movement
+    public PhysicsMaterial2D physmat_normal, physmat_springs;
+    private float radius = 0.5f;
     private Vector2 start_pos;
     private Vector2 prev_pos;
     private float speed, normal_speed = 20f;
 
+    // Warp
+    private float warp_secs = 1f;
+    private Queue<Vector2> pos_history, velocity_history;
+
     // Other State
     private bool chaser = false;
-    private Power power = Power.None;
+    private Power power = Power.Warp;
     
     // Events
     public Action<Chara, Chara> on_tag;
@@ -35,6 +44,12 @@ public class Chara : MonoBehaviour
     public bool IsChaser()
     {
         return chaser;
+    }
+    public KeyCode GetActionKeyCode()
+    {
+        return 
+            control_scheme == ControlScheme.Arrows ? KeyCode.Slash :
+            control_scheme == ControlScheme.WASD ? KeyCode.Q : KeyCode.None;
     }
 
 
@@ -58,6 +73,9 @@ public class Chara : MonoBehaviour
         rb.velocity = Vector2.zero;
         rb.angularVelocity = 0;
         speed = normal_speed;
+
+        pos_history = new Queue<Vector2>();
+        velocity_history = new Queue<Vector2>();
     }
     public void SetChaser()
     {
@@ -79,7 +97,7 @@ public class Chara : MonoBehaviour
     }
     private void Update()
     {
-        if (GetInputPower()) UsePower();
+        if (Time.timeScale > 0 && GetInputPower()) UsePower();
     }
     private void FixedUpdate()
     {
@@ -88,6 +106,12 @@ public class Chara : MonoBehaviour
 
         rb.AddForce(dir * speed, ForceMode2D.Force);
         prev_pos = transform.position;
+
+        // Warp history
+        pos_history.Enqueue(prev_pos);
+        velocity_history.Enqueue(rb.velocity);
+        if (pos_history.Count > warp_secs / Time.fixedDeltaTime) pos_history.Dequeue();
+        if (velocity_history.Count > warp_secs / Time.fixedDeltaTime) velocity_history.Dequeue();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -120,17 +144,80 @@ public class Chara : MonoBehaviour
 
     private void UsePower()
     {
-        if (power == Power.Dash)
-        {
-            StartCoroutine(Dash());
-        }
-        power = Power.None;
+        if (power == Power.Dash) StartCoroutine(Dash());
+        else if (power == Power.Blink) Blink();
+        else if (power == Power.Cloak) StartCoroutine(Cloak());
+        else if (power == Power.Repel) Repel();
+        else if (power == Power.Springs) StartCoroutine(Springs());
+        else if (power == Power.Swap) Swap();
+        else if (power == Power.Warp) Warp();
+
+        if (!unlim_powers) power = Power.None;
     }
     private IEnumerator Dash()
     {
         speed = normal_speed * 5f;
         yield return new WaitForSeconds(0.25f);
         speed = normal_speed;
+    }
+    private void Blink()
+    {
+        Vector2 dir = ((Vector2)transform.position - prev_pos).normalized;
+        //Vector2 dir = GetInputMove();
+        float dist = 5;
+        Vector2 pos = (Vector2)transform.position + dir * dist;
+
+        while (dist > 0 && Physics2D.OverlapCircle(pos, radius))
+        {
+            dist -= 0.1f;
+            pos = (Vector2)transform.position + dir * dist;
+        }
+        if (dist <= 0)
+        {
+            // Fail
+        }
+        else
+        {
+            // Success
+            transform.position = pos;
+            //rb.velocity = dir * rb.velocity.magnitude;
+        }
+        
+    }
+    private void Swap()
+    {
+        Chara opponent = GameManager.Instance.charas[1 - PlayerID];
+        Vector2 pos = transform.position;
+        transform.position = opponent.transform.position;
+        opponent.transform.position = pos;
+    }
+    private void Repel()
+    {
+        Chara opponent = GameManager.Instance.charas[1 - PlayerID];
+        Vector2 v = opponent.transform.position - transform.position;
+        float dist = Mathf.Max(radius * 2f, v.magnitude);
+        float force = 200f / Mathf.Pow(dist, 2);
+        opponent.rb.AddForceAtPosition(v.normalized * force, transform.position, ForceMode2D.Impulse);
+    }
+    private void Warp()
+    {
+        transform.position = pos_history.Peek();
+        rb.velocity = velocity_history.Peek();
+    }
+    public IEnumerator Cloak()
+    {
+        graphics.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3);
+        graphics.gameObject.SetActive(true);
+    }
+    public IEnumerator Springs()
+    {
+        Collider2D col = GetComponent<Collider2D>();
+        col.sharedMaterial = physmat_springs;
+
+        yield return new WaitForSeconds(1.5f);
+
+        col.sharedMaterial = physmat_normal;
     }
 
     private Vector2 GetInputMove()
@@ -161,13 +248,6 @@ public class Chara : MonoBehaviour
     }
     private bool GetInputPower()
     {
-        switch (control_scheme)
-        {
-            case ControlScheme.Arrows:
-                return Input.GetKeyDown(KeyCode.Slash);
-            case ControlScheme.WASD:
-                return Input.GetKeyDown(KeyCode.Q);
-        }
-        return false;
+        return Input.GetKeyDown(GetActionKeyCode());
     }
 }
--- RunnerChaserUnity/Assets/Scripts/Pickup.cs
+++ RunnerChaserUnity/Assets/Scripts/Pickup.cs
@@ -1,39 +1,64 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public enum Power { None, Dash }
+public enum Power { None, Dash, Blink, Swap, Springs, Warp, Cloak, Repel }
 
 public class Pickup : MonoBehaviour
 {
-    public Transform graphics;
-    public Power power = Power.Dash;
-
+    public Text name_text, icon_text;
+    public Power power = Power.Blink;
 
 
     private void Awake()
     {
         GameManager.Instance.on_reset += Reset;
+        Spawn();
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        graphics.gameObject.SetActive(false);
+        Chara c = collider.GetComponent<Chara>();
+        StartCoroutine(OnPickup(c));
+    }
+    private IEnumerator OnPickup(Chara c)
+    {
         GetComponent<Collider2D>().enabled = false;
+        icon_text.gameObject.SetActive(false);
+
+        // Respawn
         StartCoroutine(RespawnRoutine());
+
+        // Name text
+        KeyCode kc = c.GetActionKeyCode();
+        string kc_name = kc.ToString();
+        if (kc == KeyCode.Slash) kc_name = "/";
+        name_text.text = power.ToString().ToLower() + " [" + kc_name + "]";
+
+        for (int i = 0; i < 5; ++i)
+        {
+            name_text.gameObject.SetActive(i%2==0);
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(0.3f);
+        name_text.gameObject.SetActive(false);
     }
     private IEnumerator RespawnRoutine()
     {
         yield return new WaitForSeconds(10);
-        Respawn();
+        Spawn();
     }
-    private void Respawn()
+    private void Spawn()
     {
+        power = (Power)Random.Range(1, Tools.EnumLength(typeof(Power)));
+
         GetComponent<Collider2D>().enabled = true;
-        graphics.gameObject.SetActive(true);
+        name_text.gameObject.SetActive(false);
+        icon_text.gameObject.SetActive(true);
     }
     private void Reset()
     {
         StopAllCoroutines();
-        Respawn();
+        Spawn();
     }
 }
