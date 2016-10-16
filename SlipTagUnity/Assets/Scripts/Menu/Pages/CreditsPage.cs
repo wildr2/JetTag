dd424511ed70a071c1bc04610a5e71d30f695b73
Robert Wild <wildr2@rpi.edu>
2016-10-16 17:51:54 -0400

    Improve Credits

--- SlipTagUnity/Assets/Scripts/Menu/Pages/CreditsPage.cs
+++ SlipTagUnity/Assets/Scripts/Menu/Pages/CreditsPage.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CreditsPage : MenuPage
 {
+    public ScrollRect scroll;
+
+    protected override void Update()
+    {
+        float h = scroll.horizontalNormalizedPosition;
+        float input = Input.GetAxisRaw("Horizontal");
+
+        scroll.horizontalNormalizedPosition = 
+            Mathf.Clamp01(h + input * Time.deltaTime);
+
+        base.Update();
+    }
 }
